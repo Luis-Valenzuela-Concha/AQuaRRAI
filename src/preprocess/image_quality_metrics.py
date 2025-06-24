5d1ac5db2032c9a91783eb82895662dbb5582d42
Luis Valenzuela <lvalenzuel2020@udec.cl>
2025-06-24 10:32:41 -0400

    fix metrics formulas

--- src/preprocess/image_quality_metrics.py
+++ src/preprocess/image_quality_metrics.py
@@ -4,12 +4,12 @@ from casatools import image
 import os
 from astropy.io import fits
 import numpy as np
-from skimage.metrics import peak_signal_noise_ratio, structural_similarity
+from skimage.metrics import structural_similarity
+from math import log10, sqrt
 
 @dataclass
 class ImageQualityMetrics:
     image_path: str
-
 
     def __init__(self, image_path: str, reference_path: str = None):
         self.image_path = image_path
@@ -26,37 +26,40 @@ class ImageQualityMetrics:
             data = ia.getchunk()
             ia.close()
             data_image = data[:, :, 0, 0] if data.ndim > 2 else data
-            data_image = (data_image - data_image.min()) / (data_image.max() - data_image.min()) * 255
+            #data_image = (data_image - data_image.min()) / (data_image.max() - data_image.min()) * 255
             data_image = data_image.T[::-1, :][::-1, :]
             return data_image
 
     def __get_data_reference(self):
         if self.reference_path:
+            if not self.reference_path.endswith('.fits'):
+                raise ValueError("Reference image must be a FITS file.")
+
             path = os.path.abspath(self.reference_path)
             with fits.open(path) as hdul:
                 data_reference = hdul[0].data
             data_reference = data_reference[:, :, 0, 0] if data_reference.ndim > 2 else data_reference
-
-            data_reference = (data_reference - data_reference.min()) / (data_reference.max() - data_reference.min()) * 255
             return data_reference
         return None
 
-    def calculate_snr(self) -> float:
+    def calculate_snr(self, eps=1e-10):
         if self.data_reference is None:
             raise ValueError("Reference image is required to calculate SNR.")
         
-        eps=1e-12
-        signal = np.linalg.norm(self.data_image.flatten())
-        error = np.linalg.norm(self.data_image.flatten() - self.data_reference.flatten())
+        signal_power = np.sum(self.data_reference ** 2)
+        noise_power = np.sum((self.data_reference - self.data_image) ** 2) + eps
 
-        snr = 20 * np.log10(signal / (error + eps))
+        snr = 10 * log10(signal_power / noise_power)
         return snr
     
-    def calculate_psnr(self) -> float:
+    def calculate_psnr(self, eps = 1e-10) -> float:
         if self.data_reference is None:
             raise ValueError("Reference image is required to calculate PSNR.")
         
-        psnr = peak_signal_noise_ratio(self.data_image, self.data_reference, data_range=255)
+        mse = np.mean((self.data_image - self.data_reference) ** 2) + eps
+        max_pixel = 255.0
+
+        psnr = 20 * log10(max_pixel / sqrt(mse))
         return psnr
     
     def calculate_ssim(self) -> float:
@@ -67,7 +70,7 @@ class ImageQualityMetrics:
         return ssim
     
     def plot_data(self):
-        if self.data_reference is not None:
+        if not self.data_reference is None:
             plt.figure(figsize=(8, 4))
             plt.subplot(1, 2, 1)
             plt.imshow(self.data_image, origin='lower', cmap='inferno')
