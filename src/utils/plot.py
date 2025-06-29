ed690c2fc0e7fbd7437a531b0f78243d7acdaba3
Luis Valenzuela <lvalenzuel2020@udec.cl>
2025-06-29 12:09:21 -0400

    feat: add peak, rms and psnr

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -8,35 +8,59 @@ from astropy.io import fits
 tclean_temp = 'temp_im'
 
 def plot_image(image_path: str, zoom: int = 1, cmap: str = 'inferno', title = ''):
-    path = os.path.abspath(image_path)
+    image_name = image_path.split('/')[-1]
+
+    plot_image_path = os.path.join(image_path, f'{image_name}.image')
+    plot_residual_path = os.path.join(image_path, f'{image_name}.residual')
+    plot_model_path = os.path.join(image_path, f'{image_name}.model')
+
+    # Load image
+    image_path_abs = os.path.abspath(plot_image_path)
     ia = image()
-    ia.open(path)
-    data = ia.getchunk()
+    ia.open(image_path_abs)
+    image_data = ia.getchunk()
     ia.close()
 
-    if data.ndim == 2:
-        plt.imshow(data, origin='lower', cmap=cmap)
-    else:
-        plt.imshow(data[:, :, 0, 0], origin='lower', cmap=cmap)
-    plt.colorbar(label='Intensity')
-    
-    if title:
-        plt.title(title)
-    else:
-       plt.title(f'{path.split("/")[-1]} - {data.shape[0]}x{data.shape[1]} pixels - Zoom: {zoom}x')
+    # Load residual
+    residual_path_abs = os.path.abspath(plot_residual_path)
+    ia = image()
+    ia.open(residual_path_abs)
+    residual_data = ia.getchunk()
+    ia.close()
 
-    middle_x = data.shape[1] // 2
-    middle_y = data.shape[0] // 2
-    plt.xlim(
-        middle_x - (middle_x // zoom), 
-        middle_x + (middle_x // zoom)
-    )
-    plt.ylim(
-        middle_y - (middle_y // zoom), 
-        middle_y + (middle_y // zoom)
-    )
-    plt.xlabel('Pixel X')
-    plt.ylabel('Pixel Y')
+    model_path_abs = os.path.abspath(plot_model_path)
+    ia = image()
+    ia.open(model_path_abs)
+    model_data = ia.getchunk()
+    ia.close()
+
+    fig, axes = plt.subplots(1, 3, figsize=(8, 4))
+
+    # Plot image
+    if image_data.ndim == 2:
+        axes[0].imshow(image_data, origin='lower', cmap=cmap)
+    else:
+        axes[0].imshow(image_data[:, :, 0, 0], origin='lower', cmap=cmap)
+    axes[0].set_title(f'{image_name}.image' if not title else f'{title} (image)')
+    axes[0].axis('off')
+
+    # Plot model
+    if model_data.ndim == 2:
+        axes[1].imshow(model_data, origin='lower', cmap=cmap)
+    else:
+        axes[1].imshow(model_data[:, :, 0, 0], origin='lower', cmap=cmap)
+    axes[1].set_title(f'{image_name}.model' if not title else f'{title} (model)')
+    axes[1].axis('off')
+
+    # Plot residual
+    if residual_data.ndim == 2:
+        axes[2].imshow(residual_data, origin='lower', cmap=cmap)
+    else:
+        axes[2].imshow(residual_data[:, :, 0, 0], origin='lower', cmap=cmap)
+    axes[2].set_title(f'{image_name}.residual' if not title else f'{title} (residual)')
+    axes[2].axis('off')
+
+    plt.tight_layout()
     plt.show()
 
 def ms_to_image(ms_path: str):
@@ -46,12 +70,12 @@ def ms_to_image(ms_path: str):
     tclean(
         vis=path,
         imagename=f'{tclean_temp}/{tclean_temp}',
-        cell='0.04arcsec',
+        cell='0.02arcsec',
         imsize=256,
         niter=0,
         interactive=False
     )
-    temp_image_path = f'{tclean_temp}/{tclean_temp}.image'
+    temp_image_path = f'{tclean_temp}'
     return temp_image_path
 
 def delete_temp_image():
@@ -94,33 +118,6 @@ def plot_fits(fits_path: str, zoom: int = 1, cmap: str = 'inferno'):
     # plt.xlabel('Pixel X')
     # plt.ylabel('Pixel Y')
     # no axis
+    plt.colorbar(label='Intensity')
     plt.axis('off') # quitar
     plt.show()
-
-def get_ms_stats(ms_path: str):
-    path = os.path.abspath(ms_path)
-    if not os.path.exists(path):
-        raise FileNotFoundError(f"Measurement set not found: {path}")
-    
-    temp_image_path = ms_to_image(ms_path)
-    
-    ia = image()
-    ia.open(temp_image_path)
-    data = ia.getchunk()
-    ia.close()
-
-    delete_temp_image()
-
-    pixel_data = data[:, :, 0, 0]
-
-    # normalize pixel data
-    pixel_data = pixel_data[pixel_data != 0]  # Remove zero values to avoid division by zero
-    if pixel_data.size == 0:
-        raise ValueError("No non-zero pixel data found in the image.")
-    
-    pixel_data = (pixel_data - pixel_data.min()) / (pixel_data.max() - pixel_data.min())
-
-    return {
-        'mean': pixel_data.mean(),
-        'std': pixel_data.std()
-    }
