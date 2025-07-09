de35f4c39efeaed0f86be8f70e7ccde1c838d830
Luis Valenzuela <lvalenzuel2020@udec.cl>
2025-07-08 22:57:28 -0400

    feat: add tclean mem

--- src/preprocess/imager.py
+++ src/preprocess/imager.py
@@ -17,6 +17,9 @@ class Imager:
 
         if not vis.endswith('.ms'):
             raise ValueError("Visibility data must be a Measurement Set (.ms) file.")
+        
+        if not os.path.exists(vis):
+            raise FileNotFoundError(f"Visibility file not found: {vis}")
 
         self.vis = os.path.abspath(vis)
         self.output_path = os.path.abspath(output_path)
@@ -66,6 +69,27 @@ class Imager:
             deconvolver = 'multiscale',
             scales = scales,
             imagename= f'{self.output_path}/tclean_multiscale_{niter}/tclean_multiscale_{niter}',
+        )
+
+    def tclean_mem(self, config: dict):
+        from casatasks import tclean
+
+        imsize = config.get('imsize', 256)
+        cell = config.get('cell', '0.02arcsec')
+        niter = config.get('niter', 0)
+        weighting = config.get('weighting', 'natural')
+
+        if os.path.exists(f'{self.output_path}/tclean_mem_{niter}'):
+            os.system(f'rm -rf {self.output_path}/tclean_mem_{niter}')
+
+        tclean(
+            vis = self.vis,
+            imsize = imsize,
+            cell = cell,
+            niter = niter,
+            weighting = weighting,
+            deconvolver = 'mem',
+            imagename= f'{self.output_path}/tclean_mem_{niter}/tclean_mem_{niter}',
         )
 
     def gpuvmem(self, config: dict):
